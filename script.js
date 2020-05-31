358ea364c7a28e31f963036646b8ba07e64eea38
carlmentz <63966055+carlmentz@users.noreply.github.com>
2020-05-31 12:04:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+//question 1
+//var petArray = ["cat", "cow", "dog"];
+
+//question 2
+//console.log(petArray[1])
+
+//question 3
+//petArray.push("sheep");
+
+//question 4
+//console.log(petArray.length);
+
+//question 5
+//var catObject = {name: "Garfield", color: "orange", age: "9"};
+
+//question 6
+//console.log(catObject.age);
+
+//question 7
+//var catArray = [
+    {
+    name: "Garfield", 
+    color: "orange", 
+    age: "9"
+    }
+];
+
+//question 8
+//for(var i=0; i<catArray.length; i++){
+    console.log(catArray[i]);
+    };
+
+//question 9
+//function logToConsole(toConsole) {
+    console.log(toConsole);
+    };
+
+//question 10
+//function logToConsole(toConsole) {
+    console.log(toConsole);
+    }
+
+    toConsole("donkey");
+
+
